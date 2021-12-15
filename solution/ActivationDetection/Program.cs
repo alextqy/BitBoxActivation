08e52d64f5b9de7d847edf8bf36020fdfbb67883
alextqy <289959263@qq.com>
2021-12-15 16:58:36 +0800

    update

---
+++ solution/ActivationDetection/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ActivationDetection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
